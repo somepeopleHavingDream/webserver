aa33b1b594e6d5826b445ef00d115ae17d128873
yangxin <1393003255@qq.com>
2020-10-02 18:02:55 +0800

    feat: 实现TestClient

--- src/main/java/org/yangxin/webserver/connector/Response.java
+++ src/main/java/org/yangxin/webserver/connector/Response.java
@@ -1,6 +1,7 @@
 package org.yangxin.webserver.connector;
 
 import java.io.*;
+import java.util.Arrays;
 
 /**
  * HTTP/1.1 200 OK
@@ -24,7 +25,6 @@ public class Response {
     }
 
     public void sentStaticResource() throws IOException {
-        // todo ConnectorUtils的代码可能需要改写
         File file = new File(ConnectorUtils.WEB_ROOT, request.getRequestURI());
         try {
             write(file, HttpStatus.SC_OK);
@@ -36,9 +36,15 @@ public class Response {
     private void write(File resource, HttpStatus status) throws IOException {
         try (FileInputStream fileInputStream = new FileInputStream(resource)) {
             // 先写入响应状态
+            // 这个地方有些问题，outputStream会因为服务端系统或者程序的情况，可能会提前将缓冲区的数据发送到客户端，
+            // 特别是服务端第一次启动的时候。考虑到更妥当的做法是由客户端来做，客户端来负责接收的数据是否完整，
+            // 如果数据不完整，则客户端再次向服务端发送请求，服务端响应。
+            // 上述所说的情况在服务端启动后很少出现这种情况，但这种情况服务端应该也有更好的处理，在此先忽略，采用客户端再次请求的方式来处理。
             outputStream.write(ConnectorUtils.renderStatus(status).getBytes());
 
             byte[] buffer = new byte[BUFFER_SIZE];
+//            byte[] bytes = ConnectorUtils.renderStatus(status).getBytes();
+//            System.arraycopy(bytes, 0, buffer, 0, bytes.length);
             int length;
             while ((length = fileInputStream.read(buffer, 0, BUFFER_SIZE)) != -1) {
                 outputStream.write(buffer, 0, length);
---
+++ src/test/java/org/yangxin/webserver/TestClient.java
@@ -0,0 +1,32 @@
+package org.yangxin.webserver;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * @author yangxin
+ * 2020/10/02 17:08
+ */
+public class TestClient {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 8888);
+        OutputStream outputStream = socket.getOutputStream();
+        outputStream.write("GET /index.html HTTP/1.1".getBytes());
+        socket.shutdownOutput();
+
+        InputStream inputStream = socket.getInputStream();
+        byte[] buffer = new byte[2048];
+        int length = inputStream.read(buffer);
+        StringBuilder response = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            response.append((char) buffer[i]);
+        }
+        System.out.println(response.toString());
+        socket.shutdownInput();
+
+        socket.close();
+    }
+}
