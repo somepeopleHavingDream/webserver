96d6986bbd3ffa15cc038c6f15fd5d744d3dd6dd
yangxin <1393003255@qq.com>
2020-10-05 00:13:19 +0800

    feat: 使用nio模型重写connector

--- src/main/java/org/yangxin/webserver/connector/Connector.java
+++ src/main/java/org/yangxin/webserver/connector/Connector.java
@@ -7,8 +7,13 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.ServerSocket;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Set;
 
 /**
  * @author yangxin
@@ -18,7 +23,8 @@ public class Connector implements Runnable {
 
     private static final int DEFAULT_PORT = 8888;
 
-    private ServerSocket server;
+    private Selector selector;
+
     private final int port;
 
     public Connector() {
@@ -38,39 +44,61 @@ public class Connector implements Runnable {
     @Override
     public void run() {
         try {
-            server = new ServerSocket(port);
-            System.out.println("启动服务器，监听端口：" + port);
+            ServerSocketChannel server = ServerSocketChannel.open();
+            server.configureBlocking(false);
+            server.socket().bind(new InetSocketAddress(port));
+
+            selector = Selector.open();
+            server.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("启动服务器，监听端口：" + port + "……");
 
             while (true) {
-                // 获取客户端输入输出流
-                Socket socket = server.accept();
-                InputStream inputStream = socket.getInputStream();
-                OutputStream outputStream = socket.getOutputStream();
-
-                // 通过客户端输入流构建请求
-                Request request = new Request(inputStream);
-                request.parse();
-
-                // 通过客户端输出流和请求构建响应
-                Response response = new Response(outputStream);
-                response.setRequest(request);
-
-                // 根据请求的分类（动态请求、静态请求），交由不同的处理器类来处理
-                if (request.getRequestURI().startsWith("/servlet/")) {
-                    ServletProcessor processor = new ServletProcessor();
-                    processor.process(request, response);
-                } else {
-                    StaticProcessor processor = new StaticProcessor();
-                    processor.process(request, response);
+                selector.select();
+                Set<SelectionKey> selectionKeySet = selector.selectedKeys();
+                for (SelectionKey key : selectionKeySet) {
+                    // 处理被触发的事件
+                    handles(key);
                 }
-
-                // 关闭客户端套接字
-                close(socket);
+                selectionKeySet.clear();
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            close(server);
+            close(selector);
+        }
+    }
+
+    private void handles(SelectionKey key) throws IOException {
+        // ACCEPT
+        if (key.isAcceptable()) {
+            ServerSocketChannel server = (ServerSocketChannel) key.channel();
+            SocketChannel client = server.accept();
+            client.configureBlocking(false);
+            client.register(selector, SelectionKey.OP_READ);
+        } else {
+            // READ
+            SocketChannel client = (SocketChannel) key.channel();
+            key.cancel();
+            client.configureBlocking(true);
+            Socket clientSocket = client.socket();
+            InputStream input = clientSocket.getInputStream();
+            OutputStream output = clientSocket.getOutputStream();
+
+            Request request = new Request(input);
+            request.parse();
+
+            Response response = new Response(output);
+            response.setRequest(request);
+
+            if (request.getRequestURI().startsWith("/servlet/")) {
+                ServletProcessor processor = new ServletProcessor();
+                processor.process(request, response);
+            } else {
+                StaticProcessor processor = new StaticProcessor();
+                processor.process(request, response);
+            }
+
+            close(client);
         }
     }
 
